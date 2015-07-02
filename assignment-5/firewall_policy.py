8907a4721811de40727100a82a461137a0126dc5
Dave Lillethun <davel@cc.gatech.edu>
2015-07-02 15:03:05 -0400

    Added some extra information to the provided starter function to help students see what's going on

--- assignment-5/firewall_policy.py
+++ assignment-5/firewall_policy.py
@@ -10,11 +10,16 @@ def make_firewall_policy(config):
     # TODO - This is where you need to write the functionality to create the
     # firewall. What is passed in is a list of rules that you must implement
     # using the Pyretic syntax that was used in Assignment 2. 
+
+    # feel free to remove the following "print config" line once you no longer need it
+    print config # for demonstration purposes only, so you can see the format of the config
+
     rules = []
     for entry in config:
         # TODO - build the individual rules
 
         # examples: 
+        rule = match(dstport=entry['dstport'])
         rule = match(srcmac=MAC(entry['srcmac']))
         rule = match(srcip=entry['srcip'])
         rule = match(dstmac=MAC(entry['dstmac']), srcport=entry['srcport'])
