9341c0c632d3bef9fd80400ab4c05944656e2b92
Dave Lillethun <davel@cc.gatech.edu>
2015-09-26 22:37:42 -0400

    Added blank line after each round, and made sure the current_logs reset each round so stale entries aren't accidentally repeated

--- assignment-3/helpers.py
+++ assignment-3/helpers.py
@@ -7,23 +7,28 @@
 
 "Helpers for Assignment 3."
 logfile = None
-current_logs = {}
+current_logs = None
 ALPHABETIZE = True
+
 
 def open_log(filename):
     global logfile
+    global current_logs
+
     logfile = open(filename, "w")
-    
+    current_logs = dict()
+
 
 def add_entry(switch, logstring):
     global current_logs
+
     current_logs[switch] = logstring
     print switch + ":" + logstring
+
 
 def finish_round():
     global logfile
     global current_logs
-    global ALPHEBETIZE
     
     indices = current_logs.keys()
     if ALPHABETIZE:
@@ -31,8 +36,11 @@ def finish_round():
     for index in indices:
         logfile.write(index + ":" + current_logs[index] + "\n")
 
+    logfile.write("\n")
+    current_logs = dict()
+
+
 def finish_log():
     global logfile
+
     logfile.close()
-
-
